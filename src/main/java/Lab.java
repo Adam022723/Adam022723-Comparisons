0069871805c113844c0fc5b21403eff67e43e6a4
Adam Southard <adam.southard1@outlook.com>
2023-03-02 23:51:07 +0000

    Complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -17,14 +17,18 @@
  * You'll need to know more operators to complete the remaining parts of the lab. You can read about all the 
  * boolean comparison operators here: https://www.oreilly.com/library/view/java-for-dummies/9781118239742/a17.html
  */
-public class Lab {
+public class Lab 
+{
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is equal to b, false otherwise.
      */
-    public boolean isEqual(int a, int b){
-        return false;
+    public boolean isEqual(int a, int b)
+    {
+        if(a == b)
+        return true;    
+        else return false;
     }
 
     /**
@@ -32,32 +36,44 @@ public class Lab {
      * @param b the second number to be compared.
      * @return true if a is greater than b, false otherwise.
      */
-    public boolean greaterThan(int a, int b){
-        return false;
+    public boolean greaterThan(int a, int b)
+    {
+        if(a > b)
+        return true;
+        else return false;
     }
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is less than b, false otherwise.
      */
-    public boolean lessThan(int a, int b){
-        return false;
+    public boolean lessThan(int a, int b)
+    {   
+        if(a < b)
+        return true;
+        else return false;
     }
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is greater than or equal to b, false otherwise.
      */
-    public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+    public boolean greaterThanOrEqualTo(int a, int b)
+    {
+        if(a >= b)
+        return true;
+        else return false;
     }
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is less than or equal to b, false otherwise.
      */
-    public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+    public boolean lessThanOrEqualTo(int a, int b)
+    {
+        if(a <= b)
+        return true;
+        else return false;
     }
 
     /**
@@ -65,7 +81,10 @@ public class Lab {
      * @param b the second number to be compared.
      * @return true if a is not equal to b, false otherwise.
      */
-    public boolean notEqualTo(int a, int b){
-        return false;
+    public boolean notEqualTo(int a, int b)
+    {
+        if(a != b)
+        return true;
+        else return false;
     }
 }
